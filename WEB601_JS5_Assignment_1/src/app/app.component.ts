466a0f483695db69e4b0a0c938d1ad6f696e6b36
NidhikumariVadodariya <NV29@myscc.ca>
2023-02-03 15:06:51 -0500

    First Commit after Angular CLI files installed
    First Assignment angular CLI files downloaded

---
+++ WEB601_JS5_Assignment_1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'WEB601_JS5_Assignment_1';
+}
