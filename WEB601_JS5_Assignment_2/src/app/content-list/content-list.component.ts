fc6b1dda2b75fb0da768d324820608e9d6748fd9
NidhikumariVadodariya <NV29@myscc.ca>
2023-02-17 17:30:44 -0500

    Commit Finall

--- WEB601_JS5_Assignment_2/src/app/content-list/content-list.component.ts
+++ WEB601_JS5_Assignment_2/src/app/content-list/content-list.component.ts
@@ -31,30 +31,30 @@ export class ContentListComponent {
     this.contentArray = [
       {
         id: 1,
-        country: "Greece",
-        destination: "Santorini",
-        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-        food: "Aloo Tikki",
+        country: "India",
+        destination: "Rajasthan",
+        imgURL: "https://www.clubmahindra.com/blog/media/section_images/shuttersto-67d5d542923acf6.jpg",
+        food: "Pani-Puri",
         type: "Family trip",
-        tags: ["#goodspot"],
+        tags: ["#fabulous"],
+      },
+      {
+        id: 1,
+        country: "France",
+        destination: "Paris",
+        imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2E1p-QwK1xX0PN1CMiauhKTfxDF9R1D3-Wg&usqp=CAU",
+        food: "Paani puri",
+        type: "Friends trip",
+        tags: ["#eyecatching"],
       },
       {
         id: 2,
-        country: "Greece",
-        destination: "Santorini",
-        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-        food: "Aloo Tikki",
-        type: "Family trip",
-        tags: ["#goodspot"],
-      },
-      {
-        id: 3,
-        country: "Greece",
-        destination: "Santorini",
-        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-        food: "Aloo Tikki",
-        type: "Family trip",
-        tags: ["#goodspot"],
+        country: "Switzerland",
+        destination: "Geneva",
+        imgURL: "https://a.cdn-hotels.com/gdcs/production125/d882/347f73d7-bd57-44cf-b3fa-0837c96cb193.jpg",
+        food: "Bhel",
+        type: "Solo trip",
+        tags: ["#awesome"],
       },
       {
         id: 4,
@@ -67,174 +67,22 @@ export class ContentListComponent {
       },
       {
         id: 5,
-        country: "Greece",
-        destination: "Santorini",
-        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-        food: "Aloo Tikki",
-        type: "Family trip",
-        tags: ["#goodspot"],
+        country: "Egypt",
+        destination: "Cairo",
+        imgURL: "https://economictimes.indiatimes.com/thumb/msid-58084960,width-1200,height-900,resizemode-4,imgsize-133701/how-egypt-offers-a-captivating-canvas-of-the-past-and-the-present.jpg?from=mdr",
+        food: "Vadapav",
+        type: "Friends trip",
+        tags: ["#eyesoothing"],
       },
       {
         id: 6,
-        country: "Greece",
-        destination: "Santorini",
-        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-        food: "Aloo Tikki",
-        type: "Family trip",
-        tags: ["#goodspot"],
+        country: "Dubai",
+        destination: "Abu Dhabi",
+        imgURL: "https://www.planetware.com/wpimages/2020/11/united-arab-emirates-abu-dhabi-to-dubai-best-ways-to-get-there-by-car.jpg",
+        food: "Dabeli",
+        type: "Solo trip",
+        tags: ["#goodtrip"],
       }
     ];
   }
-
-
-
-  /*
-  contentArray: Content[] = [
-    {
-      id: 1,
-      title: 'mySampleTitle1',
-      description: 'This is a description1',
-      creator: 'Creator1',
-      imgURL: 'https://justin-biber-babysong.com',
-      type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2'],
-    },
-    {
-      id: 2,
-      title: 'mySampleTitle2',
-      description: 'This is a description2',
-      creator: 'Creator2',
-      imgURL: 'https://justin-biber-babysong.com',
-      type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2'],
-    },
-    {
-      id: 3,
-      title: 'mySampleTitle3',
-      description: 'This is a description3',
-      creator: 'Creator3',
-      imgURL: 'https://justin-biber-babysong.com',
-      type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2'],
-    },
-    {
-      id: 4,
-      title: 'mySampleTitle4',
-      description: 'This is a description4',
-      creator: 'Creator4',
-      imgURL: 'https://justin-biber-babysong.com',
-      type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2'],
-    },
-    {
-      id: 5,
-      title: 'mySampleTitle5',
-      description: 'This is a description5',
-      creator: 'Creator5',
-      imgURL: 'https://justin-biber-babysong.com',
-      type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2'],
-    },
-    {
-      id: 6,
-      title: 'mySampleTitle6',
-      description: 'This is a description6',
-      creator: 'Creator6',
-      imgURL: 'https://justin-biber-babysong.com',
-      type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2'],
-    },
-  ];
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  */
 }
-
-
-
-
-
-//import { Component } from '@angular/core';
-//import { Component, OnInit } from '@angular/core';
-//import { Content } from '../helper-files/content-interface';
-//import { ContentList } from '../helper-files/content-list';
-
-//@Component({
-//  selector: 'app-content-list',
-//  templateUrl: './content-list.component.html',
-//  styleUrls: ['./content-list.component.scss']
-//})
-//export class ContentListComponent {
-//  contentArray: Content[];
-
-//  constructor() {
-//    this.contentArray = [
-//      {
-//        id: 1,
-//        country: "Greece",
-//        destination: "Santorini",
-//        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-//        food: "Aloo Tikki",
-//        type: "Family trip",
-//        tags: ["#goodspot"],
-//      },
-//      {
-//        id: 2,
-//        country: "Greece",
-//        destination: "Santorini",
-//        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-//        food: "Aloo Tikki",
-//        type: "Family trip",
-//        tags: ["#goodspot"],
-//      },
-//      {
-//        id: 3,
-//        country: "Greece",
-//        destination: "Santorini",
-//        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-//        food: "Aloo Tikki",
-//        type: "Family trip",
-//        tags: ["#goodspot"],
-//      },
-//      {
-//        id: 4,
-//        country: "Greece",
-//        destination: "Santorini",
-//        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-//        food: "Aloo Tikki",
-//        type: "Family trip",
-//        tags: ["#goodspot"],
-//      },
-//      {
-//        id: 5,
-//        country: "Greece",
-//        destination: "Santorini",
-//        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-//        food: "Aloo Tikki",
-//        type: "Family trip",
-//        tags: ["#goodspot"],
-//      },
-//      {
-//        id: 6,
-//        country: "Greece",
-//        destination: "Santorini",
-//        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-//        food: "Aloo Tikki",
-//        type: "Family trip",
-//        tags: ["#goodspot"],
-//      },
-//      {
-//        id: 7,
-//        country: "Greece",
-//        destination: "Santorini",
-//        imgURL: "https://www.santorini-island.com/santorini-photos/information-about-santorini-santorini-island-greece-139-e01d.jpg",
-//        food: "Aloo Tikki",
-//        type: "Family trip",
-//        tags: ["#goodspot"],
-//      }
-//    ];
-//  }
-//}
