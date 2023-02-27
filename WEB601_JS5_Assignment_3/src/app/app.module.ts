427876fab6bc0c4bb50b2a4602db926fb38dbf75
NidhikumariVadodariya <NV29@myscc.ca>
2023-02-27 17:23:51 -0500

    Commit Assignment 3

---
+++ WEB601_JS5_Assignment_3/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { TypeFilterPipe } from './type-filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    //ContentCardComponent,
+    ContentListComponent,
+    TypeFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
