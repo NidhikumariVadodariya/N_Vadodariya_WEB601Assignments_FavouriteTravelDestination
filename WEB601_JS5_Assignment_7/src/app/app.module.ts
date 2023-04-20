af022e2982e69b0da90190c2741bb572108c9d0d
NidhikumariVadodariya <NV29@myscc.ca>
2023-04-20 18:36:25 -0400

    Assignment 7 commit

--- WEB601_JS5_Assignment_7/src/app/app.module.ts
+++ WEB601_JS5_Assignment_7/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './services/in-memory-data.service';
-
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FilterContentPipe } from './filter-content.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
 import { AppMessagesComponent } from './app-messages/app-messages.component';
+
 import { ModifyContentComponent } from './modify-content/modify-content.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+
 
 @NgModule({
   declarations: [
