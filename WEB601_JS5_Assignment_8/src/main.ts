ac5bbd9583d4d752bc59dae9550926dc76633c45
NidhikumariVadodariya <99766565+NidhikumariVadodariya@users.noreply.github.com>
2023-04-21 15:23:56 -0400

    Delete WEB601_JS5_Assignment_8 directory

--- WEB601_JS5_Assignment_8/src/main.ts
+++
@@ -1,9 +0,0 @@
-import 'zone.js/dist/zone'; //to open stackblitz
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
